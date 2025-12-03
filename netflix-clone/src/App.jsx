20997f1511fbed808fddc85fd9d4189df87dd4c6
Mubarekmba <mubarekmohammedb@gmail.com>
2025-12-03 12:08:24 -0800

    Header and Footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import "./index.css";
 import "./App.css";
+import Home from "./components/Pages/Home/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello</h1>
+      <Home />
     </>
   );
 }
